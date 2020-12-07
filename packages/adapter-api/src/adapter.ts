50b956998f1885dbcab6b1bd1599c547d3cbcd4a
Gilad Foyer <gilad.foyer@gmail.com>
2020-12-07 19:19:45 +0200

    Support adapter progress reporting in fetch
    JIRA: SAAS-931

--- packages/adapter-api/src/adapter.ts
+++ packages/adapter-api/src/adapter.ts
@@ -33,10 +33,21 @@ export type DeployResult = {
   errors: ReadonlyArray<Error>
 }
 
+export type Progress = {
+  phase: string
+  percents: number
+}
+
+export type ProgressReporter = {
+  reportProgress: (progress: Progress) => void
+}
+
 export type AdapterOperations = {
-  fetch: () => Promise<FetchResult>
+  fetch: (propressReporter?: ProgressReporter) => Promise<FetchResult>
   deploy: (changeGroup: ChangeGroup) => Promise<DeployResult>
 }
+
+export type AdapterOperationName = keyof AdapterOperations
 
 export type AdapterOperationsContext = {
   credentials: InstanceElement
